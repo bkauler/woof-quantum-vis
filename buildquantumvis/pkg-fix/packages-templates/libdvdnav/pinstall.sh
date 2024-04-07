f97154e72819125b87406a671f7db3fc145da2d3
bkauler <bkauler@gmail.com>
2024-04-07 08:48:23 +0800

    packages-templates: removed

--- buildquantumvis/pkg-fix/packages-templates/libdvdnav/pinstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-LIBDVDNAV="`find usr/lib -maxdepth 1 -type f -name 'libdvdnav.so.*' | head -n 1`"
-if [ "$LIBDVDNAV" ];then
- BASELIBDVDNAV="`basename $LIBDVDNAV`"
- ln -snf $BASELIBDVDNAV usr/lib/libdvdnav.so.3
- ln -snf $BASELIBDVDNAV usr/lib/libdvdnav.so.4
-fi
--- buildquantumvis/pkg-fix/packages-templates/libdvdread/pinstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-LIBDVDREAD="`find usr/lib -maxdepth 1 -type f -name 'libdvdread.so.*' 2>/dev/null | head -n 1`"
-if [ "$LIBDVDREAD" ];then
- BASELIBDVDREAD="`basename $LIBDVDREAD`"
- ln -snf $BASELIBDVDREAD usr/lib/libdvdread.so.3
- ln -snf $BASELIBDVDREAD usr/lib/libdvdread.so.8
-fi
