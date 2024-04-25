f40137b9a3a57447a74fb63154ccd75298b81739
bkauler <bkauler@gmail.com>
2024-04-25 08:57:45 +0800

    packages-templates: bluez fix

---
+++ buildquantumvis/pkg-fix/packages-templates/bluez/pinstall.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#20240423 qv
+ln -s ../libexec/bluetooth/bluetoothd usr/bin/bluetoothd
+ln -s ../libexec/bluetooth/bluetooth-meshd usr/bin/bluetooth-meshd
+ln -s ../libexec/bluetooth/obexd usr/bin/obexd
