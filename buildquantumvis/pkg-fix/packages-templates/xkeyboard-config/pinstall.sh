e0a9b9ecd539ff21aab005eff5a383161d2242b4
bkauler <bkauler@gmail.com>
2024-09-26 02:04:57 +0800

    packages-templates: ports

--- buildquantumvis/pkg-fix/packages-templates/xkeyboard-config/pinstall.sh
+++ buildquantumvis/pkg-fix/packages-templates/xkeyboard-config/pinstall.sh
@@ -5,3 +5,8 @@ cp -a -f --remove-destination usr/share/X11/xkb etc/X11/
 rm -rf usr/share/X11/xkb
 ln -s ../../../etc/X11/xkb usr/share/X11/xkb
 
+#20240619 oe scarthgap no longer has this, needed by xkbconfigmanager...
+ln -snf base.lst etc/X11/xkb/rules/xorg.lst
+#maybe also this...
+ln -snf base.xml etc/X11/xkb/rules/xorg.xml
+
