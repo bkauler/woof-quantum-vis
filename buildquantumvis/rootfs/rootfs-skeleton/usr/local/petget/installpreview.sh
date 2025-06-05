d02a10b823ff86a0c0992e86d999de88ac7c4966
bkauler <bkauler@gmail.com>
2025-06-05 10:06:18 +0800

    installpreview.sh: trivial change

--- buildquantumvis/rootfs/rootfs-skeleton/usr/local/petget/installpreview.sh
+++ buildquantumvis/rootfs/rootfs-skeleton/usr/local/petget/installpreview.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#(c) Copyright Barry Kauler 2009, puppylinux.com  QUANTUMVISMARKER
+# QUANTUMVISMARKER
+#(c) Copyright Barry Kauler 2009, puppylinux.com
 #2009 Lesser GPL licence v2 (http://www.fsf.org/licensing/licenses/lgpl.html).
 #called from pkg_chooser.sh
 #package to be previewed prior to installation is TREE1 -- inherited from parent.
